d30d77679e647776ee367708dbd398ddfa3fd963
Nadyushka <12nadya11@gmail.com>
2023-03-27 22:22:46 +0300

    friday lesson 3 start

---
+++ 3/src/features/TodolistsList/tasks-reducer.ts
@@ -0,0 +1,141 @@
+import {
+    addTodolistAC,
+    removeTodolistAC,
+    setTodolistsAC,
+} from './todolists-reducer'
+import {
+    TaskPriorities,
+    TaskStatuses,
+    TaskType,
+    todolistsAPI,
+    UpdateTaskModelType
+} from '../../api/todolists-api'
+import {Dispatch} from 'redux'
+import {AppRootStateType} from '../../app/store'
+import {setAppStatusAC} from '../../app/app-reducer'
+import {handleServerAppError, handleServerNetworkError} from '../../utils/error-utils'
+import {createAsyncThunk, createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+
+export const fetchTasksTC = createAsyncThunk('task/fetchTasksTC', async (todolistId: string, thunkAPI) => {
+    thunkAPI.dispatch(setAppStatusAC({status: 'loading'}))
+    const res = await todolistsAPI.getTasks(todolistId)
+    const tasks = res.data.items
+    thunkAPI.dispatch(setAppStatusAC({status: 'succeeded'}))
+    return {tasks, todolistId}
+})
+
+export const removeTaskTC = createAsyncThunk('task/removeTaskTC', (param: { taskId: string, todolistId: string }, thunkAPI) => {
+    return todolistsAPI.deleteTask(param.todolistId, param.taskId)
+        .then(res => {
+            // thunkAPI.dispatch(removeTaskAC({taskId: param.taskId, todolistId:param.todolistId}))
+            return {taskId: param.taskId, todolistId: param.todolistId}
+        })
+})
+
+
+const slice = createSlice({
+        name: 'task',
+        initialState: {} as TasksStateType,
+        reducers: {
+            addTaskAC(state, action: PayloadAction<{ task: TaskType }>) {
+                state[action.payload.task.todoListId].unshift(action.payload.task)
+            },
+            updateTaskAC(state, action: PayloadAction<{ taskId: string, model: UpdateDomainTaskModelType, todolistId: string }>) {
+                const tasks = state[action.payload.todolistId]
+                const index = tasks.findIndex(t => t.id === action.payload.taskId)
+                tasks[index] = {...tasks[index], ...action.payload.model}
+            },
+        },
+        extraReducers: (builder) => {
+            builder.addCase(addTodolistAC, (state, action) => {
+                state[action.payload.todolist.id] = []
+            })
+            builder.addCase(removeTodolistAC, (state, action) => {
+                delete state[action.payload.id]
+            })
+            builder.addCase(setTodolistsAC, (state, action) => {
+                action.payload.todolists.forEach(t => state[t.id] = [])
+            })
+            builder.addCase(fetchTasksTC.fulfilled, (state, action) => {
+                state[action.payload.todolistId] = action.payload.tasks
+            })
+            builder.addCase(removeTaskTC.fulfilled, (state, action) => {
+                const tasks = state[action.payload.todolistId]
+                const index = tasks.findIndex(t => t.id === action.payload.taskId)
+                state[action.payload.todolistId].splice(index, 1)
+            })
+        }
+    }
+)
+
+export const tasksReducer = slice.reducer
+
+// actions
+export const {addTaskAC, updateTaskAC} = slice.actions
+
+
+export const addTaskTC = (title: string, todolistId: string) => (dispatch: Dispatch) => {
+    dispatch(setAppStatusAC({status: 'loading'}))
+    todolistsAPI.createTask(todolistId, title)
+        .then(res => {
+            if (res.data.resultCode === 0) {
+                const task = res.data.data.item
+                const action = addTaskAC({task})
+                dispatch(action)
+                dispatch(setAppStatusAC({status: 'succeeded'}))
+            } else {
+                handleServerAppError(res.data, dispatch);
+            }
+        })
+        .catch((error) => {
+            handleServerNetworkError(error, dispatch)
+        })
+}
+export const updateTaskTC = (taskId: string, domainModel: UpdateDomainTaskModelType, todolistId: string) =>
+    (dispatch: Dispatch, getState: () => AppRootStateType) => {
+        const state = getState()
+        const task = state.tasks[todolistId].find(t => t.id === taskId)
+        if (!task) {
+            //throw new Error("task not found in the state");
+            console.warn('task not found in the state')
+            return
+        }
+
+        const apiModel: UpdateTaskModelType = {
+            deadline: task.deadline,
+            description: task.description,
+            priority: task.priority,
+            startDate: task.startDate,
+            title: task.title,
+            status: task.status,
+            ...domainModel
+        }
+
+        todolistsAPI.updateTask(todolistId, taskId, apiModel)
+            .then(res => {
+                if (res.data.resultCode === 0) {
+                    const action = updateTaskAC({taskId, model: domainModel, todolistId})
+                    dispatch(action)
+                } else {
+                    handleServerAppError(res.data, dispatch);
+                }
+            })
+            .catch((error) => {
+                handleServerNetworkError(error, dispatch);
+            })
+    }
+
+// types
+export type UpdateDomainTaskModelType = {
+    title?: string
+    description?: string
+    status?: TaskStatuses
+    priority?: TaskPriorities
+    startDate?: string
+    deadline?: string
+}
+export type TasksStateType = {
+    [key: string]: Array<TaskType>
+}
+
---
+++ 3/storybook-static/precache-manifest.99c2bd7ae5119d0d6b5289f794ceab27.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1ca0e6b0a0de9ceb373faed837a8cca",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.e4b916ccfdd5dac23247.bundle.js"
+  },
+  {
+    "url": "runtime~main.e4b916ccfdd5dac23247.bundle.js"
+  },
+  {
+    "revision": "a90e6cdf4696a179e81a",
+    "url": "static/css/main.ba1b4995.chunk.css"
+  },
+  {
+    "url": "vendors~main.e4b916ccfdd5dac23247.bundle.js"
+  },
+  {
+    "url": "vendors~main.e4b916ccfdd5dac23247.bundle.js.LICENSE.txt"
+  }
+]);
